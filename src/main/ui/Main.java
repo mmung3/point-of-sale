8132898610dff0d47b03169b468212b611dd6c07
Matthew Mung <mmung3@students.cs.ubc.ca>
2022-10-11 17:07:16 -0700

    Large changes to the UI in main. Some implementations unfinished.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,78 @@
 package ui;
 
-public class Main {
-    public static void main(String[] args) {
+import model.ProductList;
 
+import java.util.Scanner;
+
+public class Main {
+
+    private static final String REMOVE_CMD = "remove";
+    private static final String TOTAL_CMD = "total";
+    private static final String QUIT_CMD = "quit";
+
+    private ProductList productList;
+    private Scanner input;
+    private boolean running;
+    private String command;
+    private int nextId;
+    private int intInput;
+
+    public Main() {
+        System.out.println(" === WELCOME TO ____ STORE === ");
+
+        handleUserCommand();
+    }
+
+    public void handleUserCommand() {
+        input = new Scanner(System.in);
+        running = true;
+
+        while (running) {
+            if (input.hasNext()) {
+                command = input.nextLine();
+
+                try { // is the user input actually an ID?
+                    intInput = Integer.parseInt(command);
+                    understandId(intInput);
+
+                } catch (NumberFormatException e) { // otherwise, it is a command
+                    understandCommand(command);
+                }
+            }
+        }
+    }
+
+    // todo: implementation
+    public void understandCommand(String command) {
+        if (command.length() > 0) {
+            if (command.equals(REMOVE_CMD)) {
+                handleRemove();
+            } else if (command.equals(TOTAL_CMD)) {
+                handleTotal();
+            } else if (command.equals(QUIT_CMD)) {
+                running = false;
+            } else {
+                System.out.println("Invalid command, try again: ");
+            }
+        }
+    }
+
+    // todo: implement
+    public void handleRemove() {
+        //...
+    }
+
+    // todo: implement
+    public void handleTotal() {
+        // ...
+    }
+
+    // ToDo: implementation
+    public void understandId(int id) {
+        // ...
+    }
+
+    public static void main(String[] args) {
+        new Main();
     }
 }
--- src/test/model/PurchaseTest.java
+++ src/test/model/PurchaseTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PurchaseTest {
+class PurchaseTest {
 
     private Purchase purchase200Card;
     private Purchase purchase25Cash;
